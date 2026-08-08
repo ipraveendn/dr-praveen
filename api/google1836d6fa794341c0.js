d01d72996f4ddb7170ff39ee8c5d48fb42225029
PranathiReganti <pranathireganti@gmail.com>
2026-08-08 18:44:01 +0530

    Add serverless function to serve verification file without redirect

---
+++ api/google1836d6fa794341c0.js
@@ -0,0 +1,5 @@
+export default function handler(req, res) {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.setHeader('Cache-Control', 'public, max-age=86400');
+  res.status(200).send('google-site-verification: google1836d6fa794341c0');
+}
